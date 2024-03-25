f55887627067ef41862f684e7d358a8a813bd977
winterbitia <amoes.noland@gmail.com>
2024-03-25 19:40:57 +0700

    Admin logins added

--- soal_2/login.sh
+++ soal_2/login.sh
@@ -42,12 +42,65 @@ adduser() {
 
     # Add to users.txt
     echo "$email:$uname:$sec_q:$sec_a:$encrypt" >> users.txt
-    echo -e "\nREGISTRATION SUCCESSFUL!\n "
-    echo "$(date '+[%d/%m/%y %H:%M:%S]') [REGISTER SUCCESS] user [$uname] register success" >> auth.log
+    echo -e "\nAdd successful.\n " && admin
 }
 
+edituser () {
+    # Email check
+    echo "Enter your email:" && read email
+    if ! grep -q "^$email:" users.txt; then    
+        echo -e "\nEmail not found."
+        return 1
+    else
+        # Add new data
+        echo "Enter new username:" && read new_uname
+        echo "Enter new security question:" && read new_sec_q
+        echo "Enter new security answer:" && read new_sec_a
+        echo "Enter new password:" && read -s new_pass #Admins can break password rules
+        newencrypt=`echo $new_pass | base64`
+
+        # Replace old data
+        sed -i "/^$email:/c\\$email:$new_uname:$new_sec_q:$new_sec_a:$newencrypt" users.txt
+        echo -e "\nEdit successful.\n" && admin
+    fi
+}
+
+deleteuser () {
+    # Email check
+    echo "Enter your email:" && read email
+    if ! grep -q "^$email:" users.txt; then    
+        echo -e "\nEmail not found."
+        return 1
+    else
+        # Delete selected account
+        sed -i "/^$email:/d" users.txt
+        echo -e "\nDelete successful.\n" && admin
+    fi
+}
 
 admin() {
+    # Start admin module
+    echo -e "\nWelcome! You have admin privileges."
+    echo "Admin Menu"
+    echo "1. Add User"
+    echo "2. Edit User"
+    echo "3. Delete User"
+    echo "4. Exit"
+    
+    echo -e "\nChoose an option: "
+    read opt
+
+    # Select option
+    case $opt in
+    1) adduser ;;
+    2) edituser ;;
+    3) deleteuser ;;
+    4) echo "Exiting..."
+        exit 0 ;;
+    *) echo "Invalid option"
+        admin ;;
+    esac
+    
     return 0
 }
 
@@ -76,7 +129,7 @@ loginuser() {
         # Member login
         elif grep -q "^$email:.*:.*:.*:$encrypt" users.txt; then
                 echo -e "\nLogin successful\n"
-                echo "$(date '+[%d/%m/%y %H:%M:%S]') [LOGIN SUCCESS] user [$usr] login success" >> auth.log
+                echo "$(date '+[%d/%m/%y %H:%M:%S]') [LOGIN SUCCESS] user [$uname] login success" >> auth.log
                 member
         # Fail login
         else
@@ -94,7 +147,7 @@ i_forgor() {
         echo -e "\nEmail not found."
         return 1
     else
-        sec_q=`awk -F":" -v email=$email '$0~email {print $3}' users.txt`
+        sec_q=`grep "^$email:" users.txt | cut -d':' -f3`
         echo "Security question: $sec_q"
 
         echo "Enter your answer:" && read sec_a
@@ -102,7 +155,7 @@ i_forgor() {
             echo -e "\nIncorrect answer."
             return 1
         else
-            encrypt=`awk -F":" -v email=$email '$0~email {print $5}' users.txt`
+            encrypt=`grep "^$email:" users.txt | cut -d':' -f5`
             pass=`echo $encrypt | base64 -d`
             echo -e "\nYour password is: $pass\n"
         fi
@@ -114,11 +167,12 @@ i_forgor() {
 echo "Welcome to the Login Page. Please select an option:"
 echo "1. Login"
 echo "2. Forgot Password"
-echo "Choose an option: " && read opt
+echo "Choose an option: " && read locked
 
 # PASSWORD TEST == En!gma26
 
-case $opt in
+# Select option
+case $locked in
 1) loginuser ;;
 2) i_forgor ;;
 *) echo "Invalid option" ;;
